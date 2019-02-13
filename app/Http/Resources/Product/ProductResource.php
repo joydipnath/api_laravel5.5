a791728920a7ee90f73968e4293ccbf1591110c2
joydipnath <joydipsworld@gmail.com>
2019-02-13 14:38:53 +0530

    api response of product detail has been modified and improved

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -15,12 +15,21 @@ class ProductResource extends Resource
     public function toArray($request)
     {
         return [
+            
+            // We can change the return variable name for loose coupling. i.e. 'details' to 'description'. 
+            // so that in future if we change column name detail to something else then our API end users should not ger affected
 
             'name' => $this->name,
-            'description' => $this->detail,
+            'description' => $this->detail, 
             'price' => $this->price,
-            'stock' => $this->stock,
+            'stock' => $this->stock == 0 ? 'Out of stock': $this->stock,
+            'total_price' => round( (1 - ( $this->discount / 100)) * $this->price, 2),
             'discount' => $this->discount,
+            'rating' => $this->reviews()->count() > 0 ? round($this->reviews->sum('star')/$this->reviews()->count(), 2) : 'No review yet',
+            'href' => [
+                
+                'reviews' => route('reviews.index', $this->id)
+            ]
 
         ];
     }

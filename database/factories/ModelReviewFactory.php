0fe0cceaebdf9ad9372aa911a982aac06c31a654
joydipnath <joydipsworld@gmail.com>
2019-02-13 12:05:35 +0530

    migration and faker is also integrated

--- database/factories/ModelReviewFactory.php
+++ database/factories/ModelReviewFactory.php
@@ -1,9 +1,15 @@
 <?php
 
 use Faker\Generator as Faker;
+use App\Model\Product;
 
 $factory->define(App\Model\Review::class, function (Faker $faker) {
     return [
-        //
+        'customer' => $faker->name,
+        'review' => $faker->paragraph,
+        'star' => $faker->numberBetween(0,5),
+        'product_id' => function(){
+        	return Product::all()->random();
+        }
     ];
 });
